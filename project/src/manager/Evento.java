57a4c2882e90f023149716f9385cfadc28b3cdb2
diogoalves10 <a79751@alunos.uminho.pt>
2021-02-21 15:40:17 +0000

    sets

--- project/src/manager/Evento.java
+++ project/src/manager/Evento.java
@@ -4,17 +4,17 @@ import org.snmp4j.smi.*;
 
 public class Evento{
     private String nome;
-    private int duracao;
+    private Integer duracao;
     private OctetString deltaT;
     private OctetString deltaLimite;
-    boolean passou;
+
 
     public Evento(){
         nome = "";
         duracao = 0;
         deltaT = new OctetString();
         deltaLimite = new OctetString();
-        passou = false;
+
     }
 
     public Evento(String n, int d, OctetString dt, OctetString dl, boolean p){
@@ -22,20 +22,19 @@ public class Evento{
         duracao = d;
         deltaT = new OctetString(dt);
         deltaLimite = new OctetString(dl);
-        passou = p;
+
     }
 
     public String getNome(){ return nome; }
-    public int getDuracao(){ return duracao; }
+    public Integer getDuracao(){ return duracao; }
     public OctetString getDeltaT(){ return new OctetString(deltaT); }
     public OctetString getDeltaLimite(){ return new OctetString(deltaLimite); }
-    public boolean getPassou(){ return passou; }
+
 
     public void setNome(String n){ nome = n; }
     public void setDuracao(int d){ duracao = d; }
     public  void setDeltaT(OctetString dt){ deltaT = new OctetString(dt); }
     public  void setDeltaLimite(OctetString dl){ deltaT = new OctetString(dl); }
-    public void setPassou(boolean p){ passou = p; }
 
     public void decrDucacao(){ duracao--; }
 }
--- project/src/manager/Manager.java
+++ project/src/manager/Manager.java
@@ -9,11 +9,19 @@ import org.snmp4j.agent.mo.snmp.DateAndTime;
 import org.snmp4j.event.ResponseEvent;
 import org.snmp4j.mp.SnmpConstants;
 import org.snmp4j.smi.*;
+import org.snmp4j.transport.DefaultUdpTransportMapping;
+import org.snmp4j.util.DefaultPDUFactory;
+import org.snmp4j.util.TreeEvent;
+import org.snmp4j.util.TreeUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
 
 public class Manager {
     private Snmp snmp = null;
+
     //ler do ficheiro de configuração
     //adicionar novos eventos
 
@@ -27,10 +35,74 @@ public class Manager {
      */
 
     /*Falta :
-    Ir buscar os eventos à mib para ver o Id de último evento
+    Ir buscar os eventos à mib para ver o Id de último evento -> feito com o getBULK
     Acabar a função insert
     Função de procurar um evento especifico dando o nome do evento?
      */
+
+    OID[] OIDsEventosTable = {
+            new OID(new int[] {1,3,6,1,4,1,1,1}), // OID tabela de eventos
+    };
+
+    OID[] OIDsEventosTableId = {
+            new OID(new int[] {1,3,6,1,4,1,1,1,1,1}), // OID tabela de eventos
+    };
+    OID[] OIDsEventosTableNome = {
+            new OID(new int[] {1,3,6,1,4,1,1,1,1,2}), // OID tabela de eventos
+    };
+
+    OID[] OIDsEventosTableDuracao = {
+            new OID(new int[] {1,3,6,1,4,1,1,1,1,3}), // OID tabela de eventos
+    };
+
+    OID[] OIDsEventosTableDeltaT = {
+            new OID(new int[] {1,3,6,1,4,1,1,1,1,4}), // OID tabela de eventos
+    };
+
+    OID[] OIDsEventosTableDataLimite = {
+            new OID(new int[] {1,3,6,1,4,1,1,1,1,5}), // OID tabela de eventos
+    };
+
+    OID[] OIDsEventosTablePassou = {
+            new OID(new int[] {1,3,6,1,4,1,1,1,1,6}), // OID tabela de eventos
+    };
+
+    public List<VariableBinding[]> getBulk(OID[] OIDsProcesses) throws IOException { //GetBULK
+        DefaultPDUFactory pduFactory = new DefaultPDUFactory(PDU.GETBULK);
+        Snmp snmp = new Snmp(new DefaultUdpTransportMapping());
+        snmp.listen(); // open port to receive response
+        TreeUtils tree = new TreeUtils(snmp, pduFactory);
+        tree.setMaxRepetitions(100); // 100 is the max
+
+        List<TreeEvent> listWalk = tree.walk(getTargetForWrite(), OIDsProcesses);
+        List<VariableBinding[]> vbs = new ArrayList<>(listWalk.size());
+
+        int errorStatus = PDU.noError;
+        for (TreeEvent treeEvent : listWalk) {
+            errorStatus = treeEvent.getStatus();
+            if (errorStatus == PDU.noError)  // check for errors
+                vbs.add(treeEvent.getVariableBindings()); // copying the results to a data collection that can be manipulated later
+            else
+                System.out.println("error: " +errorStatus +"\n");
+        }
+        snmp.close();
+        return vbs;
+
+    }
+
+    private TreeMap<Integer,String> vbToArray (List<VariableBinding[]> vbs) { //transforma o list de variableBinding  num treemap
+
+        TreeMap <Integer,String> array = new TreeMap<>();
+
+        for (VariableBinding[] vba : vbs) {
+            for (VariableBinding vb : vba) {
+                Integer pid = vb.getOid().last();
+                array.put(pid,vb.toValueString());
+            }
+
+        }
+        return  array;
+    }
 
     public ResponseEvent set(OID oid, String val) throws IOException
     {
@@ -73,10 +145,17 @@ public class Manager {
         throw new RuntimeException("GET timed out");
     }
 
-    public void insert(int id /*, evento */) { //insere evento na MIB
+    public void insert(Integer id , Evento evento) throws IOException { //insere evento na MIB
+        //int size= vbToArray(getBulk(OIDsEventosTable)).size() ;
+        set(OIDsEventosTableId[0],id.toString());
+        set(OIDsEventosTableNome[0],evento.getNome());
+        set(OIDsEventosTableDuracao[0],evento.getDuracao().toString());
+        set(OIDsEventosTableDeltaT[0],evento.getDeltaT().toString());
+        set(OIDsEventosTableDataLimite[0],evento.getDeltaLimite().toString());
 
-       // set();
-        
+        String passou = "";
+
+        set(OIDsEventosTablePassou[0],"");
     }
 
     private Target getTargetForWrite()
