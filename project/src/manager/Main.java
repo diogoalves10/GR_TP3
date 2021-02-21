0831b63beb90a484ee48206928d8a750243c15fc
A67645 <antonio.sergio.costa.gomes@gmail.com>
2021-02-21 17:56:32 +0000

    user interface

--- project/src/manager/Main.java
+++ project/src/manager/Main.java
@@ -10,20 +10,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
       Manager m = new Manager();
       Eventos evs = new Eventos();
-
       Scanner sc = new Scanner(System.in);
 
-      evs.loadEventos();
+      System.out.println("Benvindo ao gestor de eventos!");
 
-      for(Evento e : evs.getEventos()){
-          int id;
-          id = m.vbToArray(m.getBulk(m.OIDsEventosTable)).size();
-          m.insert(id+1,e);
+      while(true) {
+          System.out.println("O que deseja fazer?\n1 -> Adicionar eventos do ficheiro de de eventos;\n2 -> Procurar um evento;");
+          int opt = sc.nextInt();
+          switch(opt) {
+              case 1:
+                evs.loadEventos();
+
+                for (Evento e : evs.getEventos()) {
+                     int id;
+                     id = m.vbToArray(m.getBulk(m.OIDsEventosTable)).size();
+                     m.insert(id + 1, e);
+                 }
+                break;
+
+              case 2:
+                  System.out.println("Qual o nome do evento a procurar?");
+                  String nome = sc.next();
+                  m.searchEvent(nome);
+                  break;
+
+              default:
+                  System.out.println("Opção inválida");
+          }
       }
-
-      
-      System.out.println("Deseja procurar um evento? (s/n)");
-      String opt = sc.next();
-
     }
 }
