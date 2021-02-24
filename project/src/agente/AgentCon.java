5f39a58377d7d95f765f6d621e6c9b59f38a3b2d
diogoalves10 <a79751@alunos.uminho.pt>
2021-02-24 15:32:05 +0000

    updateMIB

--- project/src/agente/AgentCon.java
+++ project/src/agente/AgentCon.java
@@ -1,4 +1,59 @@
 package agente;
 
+
+import agente.snmp.GrEventsMib;
+import agente.snmp.Modules;
+import manager.*;
+import org.snmp4j.agent.mo.MOTable;
+import org.snmp4j.agent.mo.MOTableRow;
+import org.snmp4j.smi.OID;
+import org.snmp4j.smi.Variable;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+
 public class AgentCon {
+    Modules modules = new Modules();
+    GrEventsMib mib = modules.getGrEventsMib();
+    GrEventsMib.EventsMIBEntryRowFactory factoryrow;
+
+    public static final OID oidGrEventsMibTable =
+            new OID(new int[] { 1,3,6,1,4,1,1,1});
+
+    public Evento (){
+
+    }
+
+    public void updteMIB(){
+        insertEvents();
+        Eventos evs = new Eventos();
+        MOTable table = mib.getEventsMIBEntry();
+        for(Integer i=1; i<=mib.getEventsMIBEntry().getModel().getRowCount();i++){
+           MOTableRow row =  table.getModel().getRow(new OID(i.toString()));
+            Variable dt =  row.getValue(4); //deltaT
+            Variable duracao = row.getValue(3); //duracao
+            Variable dl = row.getValue(5); //dataLimite
+            Variable p = row.getValue(6); //passou
+
+        }
+    }
+
+    public void insertEvents(){ //fazer a população da mib
+        Eventos evs = new Eventos();
+        evs.loadEventos();
+
+        //Percorre o arraylist de eventos e adiciona um evento de cada vez
+        for(Evento e : evs.getEventos()){
+           if(!mib.getEventsMIBEntry().getModel().containsRow(oidGrEventsMibTable.append(e.getId())));
+        //   mib.getEventsMIBEntry().addNewRow(oidGrEventsMibTable,e.vars());
+            OID oidIndex = new OID(e.getId().toString());
+            /*
+            MOTable table  = mib.getEventsMIBEntry();
+            table.createRow(new OID("1"), e.vars());
+            */
+            mib.getEventsMIBEntry().addRow(factoryrow.createRow(oidIndex,e.vars()));
+
+        }
+    }
+
 }
--- project/src/manager/Evento.java
+++ project/src/manager/Evento.java
@@ -1,40 +1,67 @@
 package manager;
 
+import agente.snmp.GrEventsMib;
+import org.snmp4j.agent.mo.snmp.DisplayString;
 import org.snmp4j.smi.*;
 
+import java.util.GregorianCalendar;
+
 public class Evento{
+    private Integer id;
     private String nome;
     private Integer duracao;
     private String deltaT;
     private String deltaLimite;
+    private Integer passou;
 
 
     public Evento(){
+        id = 0;
         nome = "";
         duracao = 0;
         deltaT = "";
         deltaLimite = "";
+        passou =0;
 
     }
 
-    public Evento(String n, int d, String dt, String dl, boolean p){
+    public Evento(Integer i, String n, int d, String dt, String dl, Integer p){
+        id = i;
         nome = n;
         duracao = d;
         deltaT = dt;
         deltaLimite =dl;
-
+        passou = p;
     }
 
     public String getNome(){ return nome; }
     public Integer getDuracao(){ return duracao; }
     public String getDeltaT(){ return deltaT; }
     public String getDeltaLimite(){ return deltaLimite; }
+    public Integer getId() { return id; }
+    public Integer getPassou() { return passou; }
 
 
     public void setNome(String n){ nome = n; }
     public void setDuracao(int d){ duracao = d; }
     public  void setDeltaT(String dt){ deltaT = dt; }
     public  void setDeltaLimite(String dl){ deltaT = dl; }
+    public void setId(Integer id) { this.id = id; }
+    public void setDuracao(Integer duracao) { this.duracao = duracao; }
+    public void setPassou(Integer passou) { this.passou = passou; }
 
     public void decrDucacao(){ duracao--; }
+
+    public Variable[] vars(){
+        Variable[] variables = new Variable[6];
+        variables[0] = new Integer32(id);
+        variables[1] = new OctetString(nome);
+        variables[2] = new Integer32(duracao);
+        variables[3] = new OctetString(deltaT);
+        variables[4] = new OctetString(deltaLimite);
+        variables[5] = new Integer32(passou);
+
+        return variables;
+    }
+
 }
